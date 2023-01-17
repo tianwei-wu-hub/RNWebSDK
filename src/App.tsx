9d06e65db1ae0114bfed5dc31006d10ba6659e25
tianwei_wu <tianwei_wu@foxitsoftware.com>
2023-01-17 09:20:36 +0800

    WebView open http page.

---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView, ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { WebView } from 'react-native-webview';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <View style={{width: '100%', height: '100%'}}>
+        <WebView
+          // source={{ uri: 'https://www.baidu.com' }}
+          source={{ uri: 'http://10.103.4.217:9999/FoxitPDFSDKForWeb/publish/8.5.2/FoxitPDFSDKForWeb_8_5_2_Full/examples/PDFViewCtrl/basic_webViewer/' }}
+          style={{borderStyle: 'solid', borderColor: 'cyan', borderWidth: 1}}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
