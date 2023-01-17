b81876fee2f38b22a313a1659c188903072afd35
tianwei_wu <tianwei_wu@foxitsoftware.com>
2023-01-17 15:24:22 +0800

    Integrate with FoxitPDFSDKForWeb_8_5_2_Full.

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {
   Platform,
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View
 } from 'react-native';
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import { WebView } from 'react-native-webview';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  const sourceUri = (
-    Platform.OS === 'android'
-      ? 'file:///android_asset/'
-      : ''
-  ) + 'Web.bundle/index.html';
+  // const sourceUri = 'http://10.103.4.217:9999/FoxitPDFSDKForWeb/publish/8.5.2/FoxitPDFSDKForWeb_8_5_2_Full/examples/PDFViewCtrl/basic_webViewer/';
+  const sourceUri = (Platform.OS === 'android' ? 'file:///android_asset/' : '') + 'Web.bundle/index.html';
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+    <SafeAreaView>
+      <StatusBar/>
       <View style={{width: '100%', height: '100%'}}>
         <WebView
-          // source={{uri: 'http://10.103.4.217:9999/FoxitPDFSDKForWeb/publish/8.5.2/FoxitPDFSDKForWeb_8_5_2_Full/examples/PDFViewCtrl/basic_webViewer/'}}
           source={{uri: sourceUri}}
           originWhitelist={['*']}
-          // android
+          allowFileAccessFromFileURLs={true}
+          allowUniversalAccessFromFileURLs={true}
           allowFileAccess={true}
         />
       </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
